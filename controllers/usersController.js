f629e2adaa006b3120069a6add50e32495ba1752
sevdarkhorse <sevdarkhorse@protonmail.com>
2024-02-23 14:50:55 -0500

    remove logs

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,10 +3,10 @@ const User = require('../models/usersModel');
 
 exports.createUser = async (req, res) => {
     try {
-        console.log('bodyyyyy', req.body);
+        // console.log('bodyyyyy', req.body);
         const newUser = new User(req.body);
         const savedUser = await newUser.save();
-        console.log('userrrr', savedUser);
+        // console.log('userrrr', savedUser);
         res.json(savedUser);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
